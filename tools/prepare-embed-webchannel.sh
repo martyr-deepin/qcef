0f962c5c222011b55dbdf8c18afe7c3c3cacb065
xushaohua <xushaohua@deepin.com>
2017-11-10 18:50:11 +0800

    Remove prepare-embed-webchannel.sh
    Change-Id: I225afaaa3a332c1b2e01d5e1d5e5e7c982b6a0c4

--- tools/prepare-embed-webchannel.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Update debian/ configuration to build with webchannel library.
-
-sed -i -e '/webchannel/d' debian/control || exit 1
-sed -i -e 's/EMBED_WEBCHANNEL.*/EMBED_WEBCHANNEL=ON \\/' debian/rules || exit 2
